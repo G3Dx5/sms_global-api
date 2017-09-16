87f6d114545c01d2aa16de24cfceb8862012a8f0
Gabe Davis <srfb2@live.com>
2017-09-16 13:18:28 +1000

    Altered README and added argparse module version

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ The repository contains three variations on the code:
 
 sms_global_HCO.py - A 'hard coded' version that contains the sender name (whatever you supply), recipient phone number and the message already in the script.  This is useful where it wil be an automated  message that is triggered withoout user interaction, for example an event on a local machine where the reipient will always be the same. If successful the will return "Message successfully sent".  If there is a problem, will return "Message not sent. Check settings and try again".
 
-Example : ./sms_global_HCO.py   
+Example: ./sms_global_HCO.py   
+
+sms_global_ARGP.py - A variation on the code that uses the argparse module to obtain mmessage settings where -s is the sender (whatever you supply), -r is the recipient phone number and -m is the message in quottion marks. If successful the will return "Message successfully sent".  If there is a problem, will return "Message not sent. Check settings and try again".
+    
+Example: ./sms_global_ARGP.py -s me -r +61400111222 -m "Hi there"
 
 
---
+++ sms_global_ARGP.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+#=======================================================================#
+# script to send messages from feeds or sensors with smsglobal account  #
+# By Gabe. github: GaDayas                                        #
+# Version 0.3 (argparse CLI version), 27 June 2017                      #
+#=======================================================================#
+
+import urllib
+import argparse
+
+#==================================================================#
+# Using the argparse module for options.                           #
+# Example ./sms_global_ARGP.py -s me -r +61400111222 -m "Hi there" #
+#==================================================================#
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--sender", help="name of sender")
+parser.add_argument("-r", "--recipient", help="phone number of recipient")
+parser.add_argument("-m", "--message", help="message to be sent")
+args = parser.parse_args()
+
+#===============================================#
+# User / API details                            #
+#===============================================#
+
+username = "PUT_API_USERNAAME_HERE"
+password = "PUT_API_PASSWORD_HERE"
+
+#===============================================#
+# Sender, recipient & message details           #
+#===============================================#
+
+sender = args.sender
+recipient = args.recipient
+message =  args.message
+
+#===============================================#
+# Assemble the URL string here with uname,      #
+# password and all message details              #
+#===============================================#
+
+http_req = "http://www.smsglobal.com/http-api.php?action=sendsms&user="
+http_req += urllib.quote(username)
+http_req += "&password="
+http_req += urllib.quote(password)
+http_req += "&from="
+http_req += urllib.quote(sender)
+http_req += "&to="
+http_req += urllib.quote(recipient)
+http_req += "&text="
+http_req += urllib.quote(message)
+
+# print(http_req) - use this to test / validate URL string
+
+#===============================================#
+# Sending function with details and message     #
+# to be sent                                    #
+#===============================================#
+
+def sendText():
+	get = urllib.urlopen(http_req)
+	req = get.read()
+	get.close()
+	if req.find("Message accepted for delivery"):
+    		print "Message successfully sent"
+	else:
+    		print "* Message not sent. Check settings and try again *"
+
+sendText()
